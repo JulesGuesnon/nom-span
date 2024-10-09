976ac5e38d67f0b1891870dc629f1ab646b346d2
Jules Guesnon <jules.guesnon@hetic.net>
2024-10-09 22:47:35 +0900

    chore(tests): Prevent regression of compare implementation

--- tests/str.rs
+++ tests/str.rs
@@ -1,4 +1,8 @@
-use nom::{character::complete::anychar, multi::many1, IResult};
+use nom::{
+    character::complete::{anychar, not_line_ending},
+    multi::many1,
+    IResult,
+};
 use nom_span::Spanned;
 
 extern crate nom;
@@ -19,4 +23,20 @@ fn utf8_vs_ascii() {
 
     assert_eq!(utf8_after.col(), 2);
     assert_eq!(ascii_after.col(), 5);
+}
+
+// What is important in this test is the fact that it prevents a regression for the compare
+// implementation.
+// `not_line_ending` requires that `T: Compare<&'static str>`, and it wasn't working before
+#[test]
+fn can_compare_with_different_type() {
+    pub fn until_newline(input: Span) -> IResult<Span, &str> {
+        let (rest, comment_str) = not_line_ending(input)?;
+        Ok((rest, *comment_str))
+    }
+    let utf8 = Span::new("test\n", true);
+
+    let (utf8_after, _) = until_newline(utf8).unwrap();
+
+    assert_eq!(*utf8_after, "\n");
 }
